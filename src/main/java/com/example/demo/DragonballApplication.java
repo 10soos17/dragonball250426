408a634fb2379d897cdcb8ad6287ffbbbd0d6b42
10soos17 <shinsh1017@gmail.com>
2025-04-28 13:00:38 +0900

    add springboot main type

---
+++ src/main/java/com/example/demo/DragonballApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonballApplication.class, args);
+
+	}
+
+}
